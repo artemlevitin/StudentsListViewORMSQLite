c925eb9770e6c9c03ae0f14e84307b357a8b80bb
artemlevitin <levitin@live.com>
2018-09-05 14:18:24 +0300

    Layouts created

---
+++ app/src/main/java/com/example/levitin/studentslistview/newStudent.java
@@ -0,0 +1,13 @@
+package com.example.levitin.studentslistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_student);
+    }
+}
