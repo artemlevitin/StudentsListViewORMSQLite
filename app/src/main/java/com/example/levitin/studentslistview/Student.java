985283931f502691f5f1ec7f57ecdd98150f9064
artemlevitin <levitin@live.com>
2018-09-07 14:46:21 +0300

    Add metods for remove students

---
+++ app/src/main/java/com/example/levitin/studentslistview/Student.java
@@ -0,0 +1,60 @@
+package com.example.levitin.studentslistview;
+
+public class Student {
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    enum Gender {MALE,FEMALE};
+
+    private String firstName;
+    private String lastName;
+    private String nameGroup;
+
+    private Gender gender;
+    //int gend
+
+public Student(String fName, String lName, String nGroup, Gender gnd){
+    setFirstName(fName);
+    setLastName(lName);
+    setNameGroup(nGroup);
+    setGender(gnd);
+
+}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNameGroup() {
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup) {
+        this.nameGroup = nameGroup;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+       Student std = (Student)obj;
+       if(std.getLastName().equals(this.lastName))
+           return true;
+
+       return false;
+    }
+}
